a2b118ad78a2de2c0af1d22255563263ae13ba25
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-25 15:09:29 -0300

    uri params / query params

--- src/main/java/br/com/app/springbasicexample/controller/ClientController.java
+++ src/main/java/br/com/app/springbasicexample/controller/ClientController.java
@@ -1,9 +1,7 @@
 package br.com.app.springbasicexample.controller;
 
 import br.com.app.springbasicexample.model.Client;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/clients")
@@ -12,5 +10,15 @@ public class ClientController {
     @GetMapping(path = "/10")
     public Client getClient() {
         return new Client(10, "Pedro", "078.569.854-76");
+    }
+
+    @GetMapping("/{id}")
+    public Client getByIdUriParam(@PathVariable int id) {
+        return new Client(id, "João", "000.000.000-01");
+    }
+
+    @GetMapping
+    public Client getByIdQueryParam(@RequestParam(name = "id", required = true) int id) {
+        return new Client(id, "Maria", "000.000.000-01");
     }
 }
