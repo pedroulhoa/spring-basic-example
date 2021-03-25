66c3e2267c34e6d4502d2069a6f51e242f24b882
Pedro Ulhoa <pedroulhoa@live.com>
2021-03-25 14:31:38 -0300

    new endpoint return object

--- src/main/java/br/com/app/springbasicexample/controller/ClientController.java
+++ src/main/java/br/com/app/springbasicexample/controller/ClientController.java
@@ -1,20 +1,16 @@
 package br.com.app.springbasicexample.controller;
 
+import br.com.app.springbasicexample.model.Client;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class FirstController {
+@RequestMapping(path = "/clients")
+public class ClientController {
 
-    @RequestMapping(path = "/hello")
-    public String hello() {
-        return "First endpoint!";
+    @GetMapping(path = "/10")
+    public Client getClient() {
+        return new Client(10, "Pedro", "078.569.854-76");
     }
-
-    @GetMapping(path = {"/one", "/two"})
-    public String twoUrls() {
-        return "two urls!";
-    }
-
 }
---
+++ src/main/java/br/com/app/springbasicexample/model/Client.java
@@ -0,0 +1,38 @@
+package br.com.app.springbasicexample.model;
+
+public class Client {
+
+    private int id;
+    private String name;
+    private String cpf;
+
+    public Client(int id, String name, String cpf) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
